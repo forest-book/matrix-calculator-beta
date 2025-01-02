e4858d7b43ef78bb125736f390c14026e36583df
forest-book <124020491+forest-book@users.noreply.github.com>
2025-01-02 23:38:23 +0900

    fix text size

--- project/models/validator.py
+++ project/models/validator.py
@@ -9,6 +9,11 @@ class Validator:
     matrix_list = []
     for multiline in multiline_list:
         matrix_list.append([float(num) for num in multiline.split()])  # 各要素をfloatに変換
-    matrix_np = np.array(matrix_list)
-    print(matrix_np)
-    return matrix_np
+    try:
+      matrix_np = np.array(matrix_list)
+    except Exception as e:
+      print(e)
+      return e
+    else:
+      print(matrix_np)
+      return matrix_np
--- project/view/view.py
+++ project/view/view.py
@@ -5,16 +5,16 @@ eg.theme("default")
 class Interface:
     def __init__(self):
         self.input_frame = [eg.Frame("Input Matrix", [
-                [eg.Multiline(size=(30, 20), key="-Matrix-"), eg.Button("calculate", key="-Calculate-")]
+                [eg.Multiline(size=(30, 20), key="-Matrix-", font=("Monospace 12")), eg.Button("calculate", key="-Calculate-")]
             ], font=("Monospace 18"))]
         
         self.result_frame = [eg.Frame("matrix calculate result", [
-                [eg.Text("inverse"), eg.Text("", key="-Inverse-")],
-                [eg.Text("transpose"), eg.Text("", key="-Transpose-")],
-                [eg.Text("determinant"), eg.Text("", key="-Determinant-")],
-                [eg.Text("eigenvalues"), eg.Text("", key="-Eigenvalues-")],
-                [eg.Text("rank"), eg.Text("", key="-Rank-")],
-                [eg.Text("diag"), eg.Text("", key="-Diag-")]
+                [eg.Text("inverse", font=("Monospace 12")), eg.Text("", key="-Inverse-", font=("Monospace 12"))],
+                [eg.Text("transpose", font=("Monospace 12")), eg.Text("", key="-Transpose-", font=("Monospace 12"))],
+                [eg.Text("determinant", font=("Monospace 12")), eg.Text("", key="-Determinant-", font=("Monospace 12"))],
+                [eg.Text("eigenvalues", font=("Monospace 12")), eg.Text("", key="-Eigenvalues-", font=("Monospace 12"))],
+                [eg.Text("rank", font=("Monospace 12")), eg.Text("", key="-Rank-", font=("Monospace 12"))],
+                [eg.Text("diag", font=("Monospace 12")), eg.Text("", key="-Diag-", font=("Monospace 12"))]
             ], font=("Monospace 18")), eg.Button("hoge", key="-ExitApp-")]
         
         self.layout = [
